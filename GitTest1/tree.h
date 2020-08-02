347845b5acd8b2c396008ad88721de32b7bb77ef
catherinehealyy <58479939+catherinehealyy@users.noreply.github.com>
2020-08-01 23:55:49 -0400

    Update tree.h

--- GitTest1/tree.h
+++ GitTest1/tree.h
@@ -7,12 +7,10 @@
 struct Wines {
 
     std::string country, region, province, winery, variety, price, points, name, description;
-
 };
 
-
-
 class Node {
+    
 
 public:
 
@@ -21,9 +19,7 @@ public:
     std::vector<Node*> children; //the children of that node its a vector bc its unknown how many children it will have
 
     Node(std::string d) { //contructor
-
         data = d;
-
     }
 
 };
@@ -35,74 +31,43 @@ class Tree {
 private:
 
     Node* root = nullptr; //PV to hold root node
-
     Node* location = nullptr; //PV to hold location of where you are looking at
 
-
-
     //PVs to hold infromation about the wine(s) you are searching for
-
     std::string country;
-
     std::string region;
-
     std::string province;
-
     std::string winery;
-
     std::string variety;
-
     std::string price;
-
     std::string points;
-
-
 
 public:
 
     Node* makeChildOf(Node* parent, std::string data); //make a node coming from another node
-
     Node* addNode(Node* parent, std::string data); //adding a node to the tree
-
     void createTree(Wines wine); //creating the tree
-
     void checkForChildMatches(std::string data, std::string topic); //check to see if any of the children of a node match hte string youre looking for
 
     std::vector<Wines> returnWineNames(); //get a vector of the Wine structs with the inputted constraints
-
     std::vector<Wines> getName(std::string name);
-
+    
     //seting the private variable to the inputted string
-
     void setCountry(std::string c);
-
     void setRegion(std::string r);
-
     void setProvience(std::string p);
-
     void setWinery(std::string w);
-
     void setVariety(std::string v);
-
     void setPrice(std::string p);
-
     void setPoints(std::string p);
 
-
-
     //retreiving the private variable
-
     std::string getCountry();
-
     std::string getRegion();
-
     std::string getProvience();
-
     std::string getWinery();
-
     std::string getVariety();
-
     std::string getPrice();
-
     std::string getPoints();
 };
+
