18657bb713ae701ae80fe64e9949b1bc15c13752
catherinehealyy <58479939+catherinehealyy@users.noreply.github.com>
2020-08-01 23:55:20 -0400

    Update tree.cpp

--- GitTest1/tree.cpp
+++ GitTest1/tree.cpp
@@ -2,141 +2,19 @@
 //COMMENT NOTE: PV = Private Variable
 
 
-#include "tree.h"
+#include "tree.hpp"
 #include <string>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void Tree::setCountry(string c) {
-
-
-
-    country = c;
-
-}
-
-void Tree::setRegion(string r) {
-
-
-
-    region = r;
-
-}
-
-void Tree::setProvience(string p) {
-
-
-
-    province = p;
-
-}
-
-void Tree::setWinery(string w) {
-
-
-
-    winery = w;
-
-}
-
-void Tree::setVariety(string v) {
-
-
-
-    variety = v;
-
-}
-
-void Tree::setPrice(string p) {
-
-
-
-    price = p;
-
-}
-
-void Tree::setPoints(string p) {
-
-
-
-    points = p;
-
-}
-
-
-
-string Tree::getCountry() {
-
-
-
-    return country;
-
-}
-
-string Tree::getRegion() {
-
-
-
-    return region;
-
-}
-
-string Tree::getProvience() {
-
-
-
-    return province;
-
-}
-
-string Tree::getWinery() {
-
-
-
-    return winery;
-
-}
-
-string Tree::getVariety() {
-
-
-
-    return variety;
-
-}
-
-string Tree::getPrice() {
-
-
-
-    return price;
-
-}
-
-string Tree::getPoints() {
-
-
-
-    return points;
-
-}
-
-
-
 Node* Tree::makeChildOf(Node* parent, string data) {
-
-    Node* child; //create the child node
-
+    
+    Node* child = new Node(data); //create the child node with new data and connects the pointer to it
     parent->children.push_back(child); //add that child to the parents children
-
-    child = new Node(data); //point child to a new node with the data
-
     return child;
-
 }
-
 
 
 Node* Tree::addNode(Node* parent, string Data) {
@@ -144,205 +22,122 @@ Node* Tree::addNode(Node* parent, string Data) {
     vector<Node*>::iterator itr;
     for (itr = parent->children.begin(); itr != parent->children.end(); itr++) {  //iterate through parent's children
         
-        string test = (*itr)->data;
-        cout << "data" << test;
-        if ( test == Data) { //if data matches any of the children's data
-
+        if ((*itr)->data == Data) { //if data matches any of the children's data
             return (*itr); //return that Node*
-
         }
-
     }
-
+    
     //if it doesnt
-
     Node* child; //create a new Node*
-
     child = makeChildOf(parent, Data); //make it a child of parent with its data
-
     return child; //return the new node
-
 }
-
 
 
 void Tree::createTree(Wines wine) {
 
-
-
     //pointers for the info of a wine
-
     Node* country;
-
     Node* region;
-
     Node* province;
-
     Node* winery;
-
     Node* variety;
-
     Node* price;
-
     Node* points;
-
     Node* name;
-
     Node* description;
 
 
-
-
-
     if (root == nullptr) { //if there is nothing in the tree
-
+        
         root = new Node("wine"); //set the root node to wine
-
         location = root; //set location* to the root so when going through the tree you start at begining
 
-
-
         //add each level as a child of the other since the tree is empty
-
         country = makeChildOf(root, wine.country);
-
-        region = makeChildOf(country, wine.region);
-
-        province = makeChildOf(region, wine.province);
-
-        winery = makeChildOf(province, wine.winery);
-
+        province = makeChildOf(country, wine.province);
+        region = makeChildOf(province, wine.region);
+        winery = makeChildOf(region, wine.winery);
         variety = makeChildOf(winery, wine.variety);
-
         price = makeChildOf(variety, wine.price);
-
         points = makeChildOf(price, wine.points);
-
         name = makeChildOf(points, wine.name);
-
         description = makeChildOf(name, wine.description);
 
-
-
     }
-
+    
     else {
 
         //check to see if the parent already has the child as a node
-
             //if it does go on to the grandchild
-
             //if it does not make add it as a child then go on to the grandchild
 
         country = addNode(root, wine.country);
-
-        region = addNode(country, wine.region);
-
-        province = addNode(region, wine.province);
-
-        winery = addNode(province, wine.winery);
-
+        province = addNode(country, wine.province);
+        region = addNode(province, wine.region);
+        winery = addNode(region, wine.winery);
         variety = addNode(winery, wine.variety);
-
         price = addNode(variety, wine.price);
-
         points = addNode(price, wine.points);
-
         name = addNode(points, wine.name);
-
         description = addNode(name, wine.description);
-
-
-
     }
-
 }
 
 
 
-void Tree::checkForChildMatches(string data, string topic) {
+
+void Tree::checkForChildMatches(string Data, string topic) {
 
     bool foundMatch = false; //a check to see if one of the children's data matches the inputted data
-
-
-
+    Node* temp = nullptr;
+    
     vector<Node*>::iterator itr;
-
     for (itr = location->children.begin(); itr != location->children.end(); itr++) {  //iterate through parent's (location's) children
-
-        if ((*itr)->data == data) { //if data matches any of the children's data
-
-
-
-            location = (*itr); //set location to that Node*
-
+         if ((*itr)->data == Data) { //if data matches any of the children's data
+            temp = *itr;
             foundMatch = true; //a match was found
-
         }
-
     }
-
+    
     if (foundMatch == false) { //no match was found
 
-        cout << "There is no matching " << topic << " in the tree.\n Please input another option" << endl; //error message
-
+        cout << "There is no matching " << topic << " in the tree.\nPlease input another option" << endl; //error message
         string newData;
-
-        cin >> newData; //take in the new string
-
+        getline(cin, newData); //take in the new string
         checkForChildMatches(newData, topic); //go through the process again with the new string
-
     }
-
-
-
-    //set the constraints infromation into the private variables
-
-    if (topic == "country") {
-
-        setCountry(data);
-
+    else if(foundMatch == true){
+        location= temp;
+        
+        //set the constraints infromation into the private variables
+        if (topic == "country") {
+              setCountry(Data);
+          }
+          else if (topic == "region") {
+              setRegion(Data);
+          }
+          else if (topic == "province") {
+              setProvience(Data);
+          }
+          else if (topic == "winery") {
+              setWinery(Data);
+          }
+          else if (topic == "variety") {
+              setVariety(Data);
+          }
+          else if (topic == "price") {
+              setPrice(Data);
+          }
+          else if (topic == "points") {
+              setPoints(Data);
+          }
+        
+        
     }
-
-    else if (topic == "region") {
-
-        setRegion(data);
-
-    }
-
-    else if (topic == "province") {
-
-        setProvience(data);
-
-    }
-
-    else if (topic == "winery") {
-
-        setWinery(data);
-
-    }
-
-    else if (topic == "variety") {
-
-        setVariety(data);
-
-    }
-
-    else if (topic == "price") {
-
-        setPrice(data);
-
-    }
-
-    else if (topic == "points") {
-
-        setPoints(data);
-
-    }
+    
 
 }
-
-
 
 vector<Wines> Tree::returnWineNames() {
 
@@ -387,8 +182,6 @@ vector<Wines> Tree::returnWineNames() {
 
 
         if ((*itr)->children.size() > 1) { //if there is more than one wine with the same name and all other factors but two+ different descriptions you need to make two nodes with the different descriptions
-
-
 
             vector<Node*>::iterator itr2;
 
@@ -579,5 +372,65 @@ vector<Wines> Tree::getName(string name) {
 
     return wines;
 
+}
+
+
+
+
+
+void Tree::setCountry(string c) {
+    country = c;
+}
+
+void Tree::setRegion(string r) {
+    region = r;
+}
+
+void Tree::setProvience(string p) {
+    province = p;
+}
+
+void Tree::setWinery(string w) {
+    winery = w;
+}
+
+void Tree::setVariety(string v) {
+    variety = v;
+}
+
+void Tree::setPrice(string p) {
+    price = p;
+}
+
+void Tree::setPoints(string p) {
+    points = p;
+}
+
+string Tree::getCountry() {
+    return country;
+}
+
+string Tree::getRegion() {
+    return region;
+}
+
+string Tree::getProvience() {
+    return province;
+}
+
+string Tree::getWinery() {
+    return winery;
+}
+
+string Tree::getVariety() {
+    return variety;
+}
+
+string Tree::getPrice() {
+    return price;
+}
+
+string Tree::getPoints() {
+    return points;
 }
 
